1568822009e08157ff34cf0209f6f6840c2d8533
nguyenphanhoailinh <nguyenphanhoailinh27@gmail.com>
2023-06-02 21:39:53 +0700

    update

---
+++ Models/HangHoa.cs
@@ -0,0 +1,16 @@
+﻿namespace MyWebApiApp.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; } = null!;
+        public double DonGia { get; set; }
+
+        
+    }
+    public class HangHoa :HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+
+
+    }
+}
